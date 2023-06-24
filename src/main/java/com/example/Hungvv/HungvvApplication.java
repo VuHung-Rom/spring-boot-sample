772b34d5650ab989c84616e7c5d4634b29424e3e
Vũ Hùng <vuahungks@gmail.com>
2023-06-24 22:43:40 +0700

    add spring project

---
+++ src/main/java/com/example/Hungvv/HungvvApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hungvv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HungvvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HungvvApplication.class, args);
+	}
+
+}
